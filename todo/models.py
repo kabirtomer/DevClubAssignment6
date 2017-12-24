1233f2f1d4b1f7bdbf6dd935236e6426012bc4b5
Kabir <kabirtomer@gmail.com>
2017-12-24 12:26:57 +0530

    Add planned model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    author=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text=models.TextField()
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+    def __str__(self):
+        return self.text[:50]
+
--- todo/urls.py
+++ todo/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', new_topic, name='blogs'),
-    path('post/<int:pk>/edit', BlogEditView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
+    path('', new_topic, name='home'),
+    path('post/<int:pk>/edit', EditView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete', DeleteView.as_view(), name='post_delete'),
     path('accounts/signup', SignUpView.as_view(), name='signup'),
 ]
