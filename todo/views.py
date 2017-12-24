a86d95d7156ee6b7b387c7e816a40a4a4fb569a3
Kabir <kabirtomer@gmail.com>
2017-12-24 12:32:54 +0530

    Add planned views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from django.views.generic import edit
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .forms import NewPostForm
 
-# Create your views here.
+class EditView(edit.UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['text']
+
+class DeleteView(edit.DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')
+
+class SignUpView(edit.CreateView):
+    form_class = UserCreationForm
+    success_url= reverse_lazy('login')
+    template_name='signup.html'
+
+def new_topic(request):
+    user=request.user
+    object_list=Post.objects.all()
+    if request.method == 'POST':
+        form= NewPostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=user
+            post.title=''
+            post.save()
+            return redirect('home')
+    else:
+        form = NewPostForm()
+    return render(request, 'home.html', {'form' : form, 'object_list' : object_list})
